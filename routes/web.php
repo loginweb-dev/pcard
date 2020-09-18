1fabd483cbc5a659eece93477e5ec1f051870cde
jaiko92 <jaiko92.rm@gmail.com>
2020-09-18 18:52:45 -0400

    creando el frontend y backend

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
